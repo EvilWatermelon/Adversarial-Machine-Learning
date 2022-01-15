29689ab8f1710e423015a7ad4793624fd7d4407e
Jan Schroeder <29424277+EvilWatermelon@users.noreply.github.com>
2022-01-15 22:10:54 +0100

    added several folders to the framework, changed implementation.tex

--- python/case_study.py
+++ python/case_study.py
@@ -1,15 +1,18 @@
 import os
 import numpy as np
-from sklearn.svm import SVC
-# from cuml.svm import SVC
 import cv2
 import matplotlib.pyplot as plt
+
 from time import time
+from sklearn.svm import SVC
+from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import *
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score, precision_score, classification_report
 from joblib import parallel_backend
+
+from art.attacks.poisoning import PoisoningAttackBackdoor, PoisoningAttackCleanLabelBackdoor
 
 
 # Declaring variables
@@ -28,12 +31,9 @@ CATEGORIES = ["0", "1", "2", "3", "4", "5", "6",
 CATEGORIES = ["0", "1"]
 DATADIR = r"C:\Users\Jan\Documents\dev\Adversarial-Machine-Learning\python\dataset\Train"
 TEST_DATADIR = r"C:\Users\Jan\Documents\dev\Adversarial-Machine-Learning\python\dataset\Test"
-IMG_SIZE=100
+IMG_SIZE = 100
 
 training_data=[]
-
-n_cpu = os.cpu_count()
-print("Number of CPUs in the system:", n_cpu)
 
 def create_training_data():
 
@@ -42,12 +42,9 @@ def create_training_data():
         class_num = CATEGORIES.index(category)
 
         for img in os.listdir(path):
-            try:
-                img_array = cv2.imread(os.path.join(path, img))
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([new_array, class_num])
-            except Exception as e:
-                pass
+            img_array = cv2.imread(os.path.join(path, img))
+            new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+            training_data.append([new_array, class_num])
 
 create_training_data()
 
@@ -62,28 +59,34 @@ for categories, label in training_data:
     X.append(categories)
     y.append(label)
 
-X = np.array(X).reshape(lenofimage,-1)
+X = np.array(X).reshape(lenofimage, -1)
 X = X/255.0
 
 y = np.array(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 print("Start training...")
+
 with parallel_backend('threading', n_jobs=8):
-	svc = make_pipeline(StandardScaler(), SVC(kernel='linear', gamma='auto'))
+	svc = make_pipeline(StandardScaler(), SVC(kernel='linear', gamma='auto', C=3000))
 
 	start = time()
 	print(f"Start time: {start}")
 
-	svc.fit(X_train, y_train)
+	clf = svc.fit(X_train, y_train)
 
 	end = time()
 	print(f"End time: {end}")
 	result = end - start
 	print('%.3f seconds' % result)
 
-	y2 = svc.predict(X_test)
+	y_pred = clf.predict(X_test)
 
-	print("Accuracy on unknown data is",accuracy_score(y_test, y2))
-	print(classification_report(y_test, y2))
+	print("Accuracy on unknown data is", accuracy_score(y_test, y_pred))
+	print(classification_report(y_test, y_pred))
+	print(f"Precision: {metrics.precision_score(y_test, y_pred, average='weighted')}")
+
+	# plot_confusion_matrix(clf, X_test, y_test)
+	# plot_det_curve(clf, X_test, y_test, average='weighted')
+	# plt.show()
---
+++ python/rmf/attacks/art/backdoors.py
@@ -0,0 +1,8 @@
+from art.attacks.poisoning import PoisoningAttackBackdoor, PoisoningAttackCleanLabelBackdoor
+
+# Executing the PoisoningAttackCleanLabelBackdoor attack
+def clean_label():
+
+
+# Executing the PoisoningAttackBackdoor 
+def art_poison_backdoor_attack():
