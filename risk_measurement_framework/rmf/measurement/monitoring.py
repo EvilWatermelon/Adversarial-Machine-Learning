d7ae4a66a41fd6087955d7436d2218298d7c8f28
Jan Schröder <526473+gb96@users.noreply.github.com>
2022-05-22 16:49:02 +0200

    backup

--- risk_measurement_framework/rmf/measurement/monitoring.py
+++ risk_measurement_framework/rmf/measurement/monitoring.py
@@ -4,6 +4,8 @@ import psutil
 from gpuinfo import GPUInfo
 from metrics.log import *
 
+counter = 0
+
 class Attacker:
     # Computational resources
     def start_ram_monitoring(self):
@@ -47,46 +49,76 @@ class Attacker:
         log(f"Percent: {min_percent}%, GPU memory: {min_memory}")
 
     def clean_label(self):
+
+        log("Clean Label Backdoor Attack")
+
         log("")
 
     def hidden_trigger(self):
+
+        log("Hidden Trigger Backdoor Attack")
+
+
         log("")
 
     def pattern_backdoor(self):
-        log("")
+        log("Pattern Backdoor Attack")
 
     def attackers_knowledge(self, attack):
 
-        if attack is "clean_label":
-            self.clean_label()
+        log("Measuring attacker's knowledge...")
 
-        elif attack is "hidden_trigger":
-            self.hidden_trigger()
+        attacks = {
+            "clean_label": self.clean_label,
+            "hidden_trigger": self.hidden_trigger,
+            "pattern_backdoor": self.pattern_backdoor
+        }
 
-        else:
-            self.pattern_backdoor()
+        attacks[attack]()
 
     def attackers_goal(self):
         log("")
 
 class Attack:
-    def positive_negative_label(self):
-        log("")
+    def positive_negative_label(self, thresholds=None, name=None, dtype=None):
+        tp = tf.keras.metrics.TruePositives(thresholds, name, dtype)
+        tn = tf.keras.metrics.TrueNegatives(thresholds, name, dtype)
+        fp = tf.keras.metrics.FalsePositives(thresholds, name, dtype)
+        fn = tf.keras.metrics.FalseNegatives(thresholds, name, dtype)
 
-    def attack_time(self):
+        return tp, tn, fp, fn
+
+
+    def attack_time(self, attack):
+        """
+        0: training times
+        1: test time
+        """
+
+        attacks = {
+            "clean_label": 0,
+            "hidden_trigger": 0,
+            "pattern_backdoor": 0
+        }
+
         log("")
 
     def accuracy_log(self, true_values, predictions, normalize=False):
 
 	    accuracy = np.sum(np.equal(true_values, predictions)) / len(true_values)
+        normalization = np.mean(accuracy)
 
 	    if normalize:
 		    log(f"Accurary: {accuracy}")
+            return accuracy
 	    else:
-		    log(f"Normalized accurary: {np.mean(accuracy)}")
+		    log(f"Normalized accurary: {normalization}")
+            return normalization
 
-    def attack_specificty(self):
-        log("")
-
-    def training_data(x):
-        log("")
+    def attack_specificty(self, target=False):
+        if target:
+            log("Untargeted attack")
+            return 0
+        else:
+            log("Targeted attack")
+            return 1
